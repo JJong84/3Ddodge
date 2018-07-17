ff998c8132ee5cceffb254f8ec9bbb5207147d0d
24seconds <24crazyoung@gmail.com>
2018-07-18 01:43:38 +0900

    Make playerCamera Smooth very works well! by jyeongho

--- Assets/ThirdPersonCamera.cs
+++ Assets/ThirdPersonCamera.cs
@@ -9,70 +9,43 @@ public class ThirdPersonCamera : MonoBehaviour {
     public bool useOffsetValues;
     public float rotateSpeed;
     private const float Y_ANGLE_MIN = -50.0f;
-    private const float Y_ANGLE_MAX = 50.0f;
-    public Transform pivot;
+    private const float Y_ANGLE_MAX = 50.0f;    
+    public float horizontal;
+    public float vertical;
+
+
 
     void Start()
     {
-        if(!useOffsetValues)
+        if (!useOffsetValues)
         {
             offset = target.position - transform.position;
         }
 
-        pivot.transform.position = target.transform.position;
-        pivot.transform.parent = target.transform;
-        
     }
 
     void Update()
     {
-        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
         target.Rotate(0, horizontal, 0);
 
-        float vertcal = Input.GetAxis("Mouse Y") * rotateSpeed;
-        target.Rotate(-vertcal, 0, 0);
-        vertcal = Mathf.Clamp(vertcal, Y_ANGLE_MIN, Y_ANGLE_MAX);
+        vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        target.Rotate(-vertical, 0, 0);
 
-        float desiredYAngle = target.eulerAngles.y;
-        float desiredXAngle = pivot.eulerAngles.x;
-        transform.Rotate(-vertcal, horizontal, 0);
-        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
-        transform.position = target.position - (rotation * offset);
-        transform.LookAt(target);
-    }
-    /*
-    private const float Y_ANGLE_MIN = -50.0f;
-    private const float Y_ANGLE_MAX = 50.0f;
-    public Transform lookAt;
-    public Transform camTransform;
-
-    private Camera cam;
-
-    private float distance = 8.0f;
-    private float currentX = 0.0f;
-    private float currentY = 0.0f;
-    private float sensivityX = 10.0f;
-    private float sensivityY = 10.0f;
-
-    private void Start()
-    {
-        camTransform = transform;
-        cam = Camera.main;
-    }
-
-    private void Update()
-    {
-        currentX += Input.GetAxis("Mouse X");
-        currentY += Input.GetAxis("Mouse Y");
-        currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+        Quaternion rotation = Quaternion.Euler(target.eulerAngles.x, target.eulerAngles.y, target.eulerAngles.z);
     }
 
     private void LateUpdate()
     {
-        Vector3 dir = new Vector3(0, 0, -distance);
-        Quaternion rotation = Quaternion.Euler(-currentY, currentX, 0);
-        camTransform.position = lookAt.position + rotation * dir;
-        camTransform.LookAt(lookAt.position);
+        Vector3 _wantedPosition = target.TransformPoint(0, 4.0f, -15.0f);
+        transform.position = Vector3.Lerp(transform.position, _wantedPosition, Time.deltaTime * 15.0f);
+
+        Quaternion _wantedRotation = Quaternion.LookRotation(target.position - transform.position, target.up);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, _wantedRotation, Time.deltaTime * 1.0f);
+
+        transform.LookAt(target, target.up);
     }
-    */
+
+
 }
